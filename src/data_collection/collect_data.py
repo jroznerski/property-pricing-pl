4198fef177e33d19dfceafb745a678446f44bc9f
37MANY <125470427+jroznerski@users.noreply.github.com>
2026-05-11 22:02:16 +0200

    pierwsze zmiany w projekcie

--- src/data_collection/collect_data.py
+++ src/data_collection/collect_data.py
@@ -2,3 +2,10 @@
 Data collection module for Polish real estate pricing system.
 Future: scraping / API integration / external datasets.
 """
+
+import kagglehub
+
+# Download latest version
+path = kagglehub.dataset_download("krzysztofjamroz/apartment-prices-in-poland")
+
+print("Path to dataset files:", path)
